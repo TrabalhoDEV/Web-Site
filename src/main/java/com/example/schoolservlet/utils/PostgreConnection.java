6e33e589d430f2f48d0a3e464132537d3bfe8ce9
Caio Marcos <caio.maciel@germinare.org.br>
2026-02-04 06:37:01 -0300

    feat: changing commments to English not in pt-br

--- src/main/java/com/example/schoolservlet/utils/PostgreConnection.java
+++ src/main/java/com/example/schoolservlet/utils/PostgreConnection.java
@@ -8,7 +8,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class PostgreConnection {
     public static Connection getConnection() {
         Dotenv dotenv = null;
-//      Tenta carregar o .env localmente
+
+        // Firstly, it tries to load .env locally
         try {
             dotenv = Dotenv.configure()
                     .ignoreIfMissing()
@@ -17,18 +18,16 @@ public class PostgreConnection {
             dotenv = null;
         }
 
-//      Busca de variáveis — primeiro do sistema para caso a aplicação esteja em produção,
-//      depois do .env local para caso esteja em desenvolvimento
 
-//        Url do banco de dados:
+        // Create variables and tries to get their values loccaly,
+        // if not it gets from system beacause it's in production maybe in Render
+
         String url = System.getenv("DB_URL");
         if (url == null && dotenv != null) url = dotenv.get("DB_URL");
 
-//        Usuário de acesso ao banco de dados:
         String usuario = System.getenv("DB_USER");
         if (usuario == null && dotenv != null) usuario = dotenv.get("DB_USER");
 
-//        Senha do banco de dados:
         String senha = System.getenv("DB_PASSWORD");
         if (senha == null && dotenv != null) senha = dotenv.get("DB_PASSWORD");
 
