a1d125e809246e37fb0bea2e5776f13212e85c0a
Caio Marcos <caio.maciel@germinare.org.br>
2026-02-04 06:37:01 -0300

    feat: changing commments to English not in pt-br

--- src/main/java/com/example/schoolservlet/daos/UserDAO.java
+++ src/main/java/com/example/schoolservlet/daos/UserDAO.java
@@ -1,5 +1,39 @@
 package com.example.schoolservlet.daos;
 
-public class UserDAO {
+import com.example.schoolservlet.models.User;
+import com.example.schoolservlet.models.enums.UserStatusEnum;
+import com.example.schoolservlet.utils.PostgreConnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+    public User findByEnrollment(int enrollment){
+        User user = null;
+        String sql = "SELECT * FROM user WHERE enrollment = ?";
+
+        try(
+            Connection conn = PostgreConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            pstmt.setInt(1, enrollment);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()){
+                user.setEnrollment(enrollment);
+                user.setCpf(rs.getString("cpf"));
+                user.setName(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                user.setStatus(UserStatusEnum.values()[rs.getInt("status")]);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
 }
--- src/main/java/com/example/schoolservlet/models/User.java
+++ src/main/java/com/example/schoolservlet/models/User.java
@@ -10,7 +10,50 @@ public class User {
     private String password;
     private String cpf;
     private UserStatusEnum status;
+
+    // Getters:
+    public int getEnrollment() {
+        return enrollment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
     public UserStatusEnum getStatus() {
         return status;
+    }
+
+    // Setters:
+    public void setEnrollment(int enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setStatus(UserStatusEnum status) {
+        this.status = status;
     }
 }
