1e2fa0a87063f923059720136926c317c3e708ed
Caio Marcos <caio.maciel@germinare.org.br>
2026-02-07 10:15:08 -0300

    feat(OutputFormat): creating a class to transform the data in database to another shape for the output

---
+++ src/main/java/com/example/schoolservlet/utils/OutputFormatService.java
@@ -0,0 +1,53 @@
+package com.example.schoolservlet.utils;
+
+/**
+ * Public class that has static methods to format the data in the database to the output.
+ */
+public class OutputFormatService {
+    /**
+     * Static method that makes the name in database to capitalize
+     * @param name Is the student or teacher name
+     * @return     name with all parts capitalized
+     */
+    public static String formatName(String name){
+        String[] nameDivided = name.split("\\s+");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < nameDivided.length; i++){
+            result.append(capitalizeOne(nameDivided[i]));
+
+            if (i < nameDivided.length - 1){
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * Private static method that capitalize a word, it's a helper in this class
+     * @param word Is the word to capitalize
+     * @return     The word capitalized
+     */
+    private static String capitalizeOne(String word){
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    }
+
+    /**
+     * Static method that capitalize the observation in the database
+     * @param obs Is the observation in database
+     * @return    The observation capitalized
+     */
+    public static String formatObs(String obs){
+        return capitalizeOne(obs);
+    }
+
+    /**
+     * Static method that makes the enrollment has 6 characters with some zeros before if needed
+     * @param enrollment Is student's enrollment
+     * @return           The enrollment with 6 characters
+     */
+    public static String formatEnrollment(int enrollment){
+        return String.format("%06d", enrollment);
+    }
+}
