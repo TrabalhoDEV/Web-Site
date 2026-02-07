bce9d49404bd9193ab3db5514ee14dff69a92f14
Eduardo Costa <eduardocosta942z@gmail.com>
2026-02-07 17:33:43 -0300

    fix: add constructor and setter for teacherPassword in Subject.java

--- src/main/java/com/example/schoolservlet/models/Subject.java
+++ src/main/java/com/example/schoolservlet/models/Subject.java
@@ -13,11 +13,15 @@ public class Subject {
 
     }
 
-    public Subject(int id, String name, String teacherName, String teacherUser){
+    public Subject(int id, String name, String teacherName, String teacherUser, String teacherPassword){
         this.id = id;
         this.name = name;
         this.teacherName = teacherName;
         this.teacherUser = teacherUser;
+        this.teacherPassword = teacherPassword;
+    }
+    public Subject(int id){
+        this.id = id;
     }
 
     // Getters
@@ -42,11 +46,6 @@ public class Subject {
     }
 
     // Setters:
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -58,4 +57,6 @@ public class Subject {
     public void setTeacherUser(String teacherUser) {
         this.teacherUser = teacherUser;
     }
+
+    public void setTeacherPassword(String teacherPassword) {this.teacherPassword = teacherPassword; }
 }
