1554c32407cb4563bbc91e4b1c5bfd955b1ced6d
Caio Marcos <caio.maciel@germinare.org.br>
2026-02-07 08:56:48 -0300

    feat(InputNormalizer): creting a class that has static methods to transform the shape of the user's input to the format defined in the business' rule

---
+++ src/main/java/com/example/schoolservlet/utils/InputNormalizer.java
@@ -0,0 +1,55 @@
+package com.example.schoolservlet.utils;
+
+/**
+ *  Class that contains static methods to normalize the user's input to the database format.
+ *  This class allows us to change the business' rule easier than if we keep that validation in
+ *  other classes like DAOs or Servlets.
+ */
+public class InputNormalizer {
+    /**
+     * Static method that normalize e-mail to lower case
+     * @param email Is user's e-mail
+     * @return      e-mail in lower case
+     */
+    public static String normalizeEmail(String email){
+        return email.toLowerCase();
+    }
+
+    /**
+     * Static method that normalize name to lower case
+     * @param name Is user's name
+     * @return      name in lower case
+     */
+    public static String normalizeName(String name){
+        return name.toLowerCase();
+    }
+
+    /**
+     * Static method that normalize student's enrollment to lower case
+     * @param enrollment Is the enrollment that every student has
+     * @return      enrollment without the initial zeros
+     */
+    public static int normalizeEnrollment(int enrollment){
+        String helper = Integer.toString(enrollment);
+        helper = helper.replaceFirst("^0+", "");
+        return Integer.parseInt(helper);
+    }
+
+    /**
+     * Static method that normalize observation to lower case
+     * @param obs Is the observation made by a teacher for a user
+     * @return    observation in lower case
+     */
+    public static String normalizeObs(String obs){
+        return obs.toLowerCase();
+    }
+
+    /**
+     * Static method that normalize userName to lower case
+     * @param userName Is the userName of the teachers
+     * @return         userName in lower case
+     */
+    public static String normalizeUserName(String userName){
+        return userName.toLowerCase();
+    }
+}
