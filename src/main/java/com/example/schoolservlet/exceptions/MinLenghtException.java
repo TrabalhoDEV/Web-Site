3cfdbe1e34f1746a240df131c5d094507c78891c
Caio Marcos <caio.maciel@germinare.org.br>
2026-02-18 07:50:46 -0300

    feat: fixing some comments, also adding the exceptions to others DAO's methods

--- src/main/java/com/example/schoolservlet/exceptions/MinLenghtException.java
+++
@@ -1,7 +0,0 @@
-package com.example.schoolservlet.exceptions;
-
-public class MinLenghtException extends ValidationException{
-    public MinLenghtException(String field, int lenght){
-        super(String.format("O campo %s deve ter menos do que %d caracteres", field, lenght), field);
-    }
-}
