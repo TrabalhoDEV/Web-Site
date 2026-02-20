bcfcdd621943a9b3a2e8c7c8c79c81fb7153768d
Caio Marcos <caio.maciel@germinare.org.br>
2026-02-19 21:21:20 -0300

    feat(teacher): creating a servlet to return all data necessarily by teacher's home page

--- src/main/java/com/example/schoolservlet/daos/interfaces/IStudentSubjectDAO.java
+++ src/main/java/com/example/schoolservlet/daos/interfaces/IStudentSubjectDAO.java
@@ -6,7 +6,6 @@ import com.example.schoolservlet.models.StudentSubject;
 import com.example.schoolservlet.utils.records.StudentsPerformance;
 import com.example.schoolservlet.utils.records.TeacherPendency;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Interface that defines the contract, like functions, exceptions and params of diferent methos implemented in {@link com.example.schoolservlet.daos.StudentSubjectDAO}
