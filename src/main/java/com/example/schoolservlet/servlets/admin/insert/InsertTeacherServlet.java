f993a29ec061a35d08e0b758fcf908845fb152be
Caio Marcos <caio.maciel@germinare.org.br>
2026-02-20 07:28:02 -0300

    feat(admin): create servlet for admin find many teachers

---
+++ src/main/java/com/example/schoolservlet/servlets/admin/insert/InsertTeacherServlet.java
@@ -0,0 +1,20 @@
+package com.example.schoolservlet.servlets.admin.insert;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "admin-insert-teacher", value = "/admin/teacher/insert")
+public class InsertTeacherServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
