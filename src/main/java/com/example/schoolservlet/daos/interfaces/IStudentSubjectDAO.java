796e8272e014b9fef6d431429a83bd250d242963
Caio Marcos <caio.maciel@germinare.org.br>
2026-02-19 21:21:20 -0300

    feat(teacher): creating a servlet to return all data necessarily by teacher's home page

--- src/main/java/com/example/schoolservlet/daos/interfaces/IStudentSubjectDAO.java
+++ src/main/java/com/example/schoolservlet/daos/interfaces/IStudentSubjectDAO.java
@@ -34,6 +34,26 @@ public interface IStudentSubjectDAO {
     int totalCount(int studentId) throws DataException;
     
     /**
+     * Retrieves a paginated collection of subjects for a specific student.
+     *
+     * @param skip the number of records to skip for pagination
+     * @param take the maximum number of records to retrieve
+     * @param studentId the unique identifier of the student
+     * @return a {@link Map} where the key is the subject ID and the value is the {@link StudentSubject} object,
+     *         or an empty map if no subjects are found
+     */
+    Map<Integer, StudentSubject> findMany(int skip, int take, int studentId) throws DataException;
+
+
+    /**
+     * Retrieves the total count of subjects associated with a specific student.
+     *
+     * @param studentId the unique identifier of the student
+     * @return the total number of subjects for the given student ID
+     */
+    int totalCount(int studentId) throws DataException;
+
+    /**
      * Method that find the students with less grade, helping teacher find who he should give more attention
      * @param teacherId is teacher id
      * @return      students, subjects and the data between tehem, all related to passed teacher
@@ -59,24 +79,4 @@ public interface IStudentSubjectDAO {
      * @throws ValidationException  if teacherId is empty or less than 0
      */
     List<TeacherPendency> teacherPendency(int teacherId) throws DataException, ValidationException;
-
-    /**
-     * Retrieves a paginated collection of subjects for a specific student.
-     *
-     * @param skip the number of records to skip for pagination
-     * @param take the maximum number of records to retrieve
-     * @param studentId the unique identifier of the student
-     * @return a {@link Map} where the key is the subject ID and the value is the {@link StudentSubject} object,
-     *         or an empty map if no subjects are found
-     */
-    Map<Integer, StudentSubject> findMany(int skip, int take, int studentId);
-
-
-    /**
-     * Retrieves the total count of subjects associated with a specific student.
-     *
-     * @param studentId the unique identifier of the student
-     * @return the total number of subjects for the given student ID
-     */
-    int totalCount(int studentId);
 }
