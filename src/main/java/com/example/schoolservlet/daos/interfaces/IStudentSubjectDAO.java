eeeb1ac9d7b64ff4c4be461af800e3e2c42acabd
Caio Marcos <caio.maciel@germinare.org.br>
2026-02-21 11:16:56 -0300

    feat: adding Dockerfile to be able to run project in Render

--- src/main/java/com/example/schoolservlet/daos/interfaces/IStudentSubjectDAO.java
+++ src/main/java/com/example/schoolservlet/daos/interfaces/IStudentSubjectDAO.java
@@ -1,5 +1,6 @@
 package com.example.schoolservlet.daos.interfaces;
 
+import com.example.schoolservlet.exceptions.DataException;
 import com.example.schoolservlet.models.StudentSubject;
 
 import java.util.Map;
@@ -19,7 +20,7 @@ public interface IStudentSubjectDAO {
      * @return a {@link Map} where the key is the subject ID and the value is the {@link StudentSubject} object,
      *         or an empty map if no subjects are found
      */
-    Map<Integer, StudentSubject> findMany(int skip, int take, int studentId);
+    Map<Integer, StudentSubject> findMany(int skip, int take, int studentId) throws DataException;
 
 
     /**
@@ -28,5 +29,5 @@ public interface IStudentSubjectDAO {
      * @param studentId the unique identifier of the student
      * @return the total number of subjects for the given student ID
      */
-    int totalCount(int studentId);
+    int totalCount(int studentId) throws DataException;
 }
