6274b5fcaf746a3a0ba30f972e8e1285d5cd8101
Eduardo Costa <112218438+EduardoCosta942@users.noreply.github.com>
2026-02-20 21:07:47 -0300

    feature: Add feedbacks dashboard and bulletin view (#64)
    * feat: add feedbacks dashboard
    
    * fix: improve pagination logic
    
    * fix: remove testing logs
    
    * feat: add bulletin view
    
    * fix: improve constants usage, redirect logic and simplify the code
    
    * fix: fix pagination bug: Infinity amount of pages

--- src/main/java/com/example/schoolservlet/daos/StudentSubjectDAO.java
+++ src/main/java/com/example/schoolservlet/daos/StudentSubjectDAO.java
@@ -39,7 +39,7 @@ public class StudentSubjectDAO implements GenericDAO<StudentSubject>, IStudentSu
                     "sb.name AS subject_name, " +
                     "sb.deadline AS subject_deadline " +
                     "FROM student_subject ss JOIN student st ON st.id = ss.id_student JOIN subject sb " +
-                    "ON sb.id = ss.subject_id ORDER BY ss.id LIMIT ? OFFSET ?")
+                    "ON sb.id = ss.id_subject ORDER BY ss.id LIMIT ? OFFSET ?")
         ){
             pstmt.setInt(1, take < 0 ? 0 : (take > Constants.MAX_TAKE ? Constants.MAX_TAKE : take));
             pstmt.setInt(2, skip < 0 ? 0 : skip);
@@ -78,6 +78,68 @@ public class StudentSubjectDAO implements GenericDAO<StudentSubject>, IStudentSu
     }
 
     @Override
+    public Map<Integer, StudentSubject> findMany(int skip, int take, int studentId) {
+        Map<Integer, StudentSubject> studentsSubjects = new HashMap<>();
+
+        try (Connection conn = PostgreConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement("SELECT " +
+                     "ss.id, " +
+                     "ss.obs, " +
+                     "ss.grade1, " +
+                     "ss.grade2, " +
+                     "st.id AS student_id, " +
+                     "st.name AS student_name, " +
+                     "st.cpf AS student_cpf, " +
+                     "st.email AS student_email, " +
+                     "sb.id AS subject_id, " +
+                     "sb.name AS subject_name, " +
+                     "sb.deadline AS subject_deadline " +
+                     "FROM student_subject ss JOIN student st ON st.id = ss.id_student JOIN subject sb " +
+                     "ON sb.id = ss.id_subject " +
+                     "WHERE st.ID = ? " +
+                     "ORDER BY ss.id LIMIT ? OFFSET ?")
+        ) {
+
+            int limit = take < 0 ? 0 : Math.min(take, Constants.MAX_TAKE);
+            int offset = Math.max(skip, 0);
+
+            pstmt.setInt(1, studentId);
+            pstmt.setInt(2, limit);
+            pstmt.setInt(3, offset);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                Student student = new Student();
+                student.setId(rs.getInt("student_id"));
+                student.setName(rs.getString("student_name"));
+                student.setCpf(rs.getString("student_cpf"));
+                student.setEmail(rs.getString("student_email"));
+                student.setStatus(StudentStatusEnum.ACTIVE);
+
+                Subject subject = new Subject();
+                subject.setId(rs.getInt("subject_id"));
+                subject.setName(rs.getString("subject_name"));
+                subject.setDeadline(rs.getDate("subject_deadline"));
+
+                StudentSubject studentSubject = new StudentSubject();
+                studentSubject.setId(rs.getInt("id"));
+                studentSubject.setObs(rs.getString("obs"));
+                studentSubject.setGrade1(rs.getDouble("grade1"));
+                studentSubject.setGrade2(rs.getDouble("grade2"));
+                studentSubject.setStudent(student);
+                studentSubject.setSubject(subject);
+
+                studentsSubjects.put(studentSubject.getId(), studentSubject);
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+
+        return studentsSubjects;
+    }
+
+    @Override
     public StudentSubject findById(int id) throws NotFoundException, DataException, ValidationException {
         InputValidation.validateId(id, "id");
 
@@ -95,7 +157,7 @@ public class StudentSubjectDAO implements GenericDAO<StudentSubject>, IStudentSu
                     "sb.name AS subject_name, " +
                     "sb.deadline AS subject_deadline " +
                     "FROM student_subject ss JOIN student st ON st.id = ss.id_student JOIN subject sb " +
-                    "ON sb.id = ss.subject_id WHERE ss.id = ?")){
+                    "ON sb.id = ss.id_subject WHERE ss.id = ?")){
             pstmt.setInt(1, id);
 
             ResultSet rs = pstmt.executeQuery();
@@ -286,6 +348,24 @@ public class StudentSubjectDAO implements GenericDAO<StudentSubject>, IStudentSu
         try(Connection conn = PostgreConnection.getConnection();
             Statement stmt = conn.createStatement()){
             ResultSet rs = stmt.executeQuery("SELECT COUNT(*) AS totalCount FROM student_subject");
+
+            if (rs.next()) {
+                return rs.getInt("totalCount");
+            }
+            return -1;
+        } catch (SQLException sqle){
+            sqle.printStackTrace();
+            throw new DataException("Erro ao contar relações entre alunos e professores", sqle);
+        }
+    }
+
+    public int totalCount(int studentId) throws DataException {
+        try(Connection conn = PostgreConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(
+                    "SELECT COUNT(*) AS totalCount FROM student_subject WHERE id_student = ?"
+            )){
+            pstmt.setInt(1, studentId);
+            ResultSet rs = pstmt.executeQuery();
 
             if (rs.next()) {
                 return rs.getInt("totalCount");
