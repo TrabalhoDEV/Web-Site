a6363b1f1c3ce439e4e934f8955040420ae4379f
Caio Marcos <caio.maciel@germinare.org.br>
2026-03-02 21:25:42 -0300

    chore: finishing to solve some merge problems

--- src/main/java/com/example/schoolservlet/utils/InputValidation.java
+++ src/main/java/com/example/schoolservlet/utils/InputValidation.java
@@ -196,7 +196,6 @@ public class InputValidation {
         if (name.length() > Constants.MAX_SUBJECT_NAME_LENGTH) throw new MaxLengthException("nome", Constants.MAX_SUBJECT_NAME_LENGTH);
         if (name.length() < Constants.MIN_SUBJECT_NAME_LENGTH) throw new MinLengthException("nome", Constants.MIN_SUBJECT_NAME_LENGTH);
     }
-<<<<<<< HEAD
 
     /**
      * Static method that validates if name contains only letters and whitespace characters.
@@ -210,19 +209,18 @@ public class InputValidation {
      * @throws RequiredFieldException if name is null or blank
      * @throws ValidationException if name contains invalid characters (numbers, special characters, symbols)
      */
-    public static void validateStudentName(String name) throws ValidationException{
+    public static void validateStudentName(String name) throws ValidationException {
         validateIsNull("nome", name);
-        for (char c : name.toCharArray()){
-            if (!Character.isLetter(c) && !Character.isWhitespace(c)){
+        for (char c : name.toCharArray()) {
+            if (!Character.isLetter(c) && !Character.isWhitespace(c)) {
                 throw new ValidationException("Nome contém caracteres inválidos");
             }
         }
-=======
+    }
     
     public static void validateName(String name) throws ValidationException{
         if (!StandardCharsets.US_ASCII.newEncoder().canEncode(name)) throw new ValidationException("Nome contém caracteres inválidos");
         if (name.matches("^[\\p{L} ]*$")) throw new RegexException("nome");
->>>>>>> 987874a (feat: creating routes to create or update school_class)
     }
 
     public static void validateSchoolClassName(String name) throws ValidationException{
