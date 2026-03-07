9db777e6e3ed4da85b3fb98f14f6704c94f2bce1
eduardocostaam <eduardocosta942z@gmail.com>
2026-03-07 11:32:18 -0300

    feat: add secret concatenation to password

--- src/main/java/com/example/schoolservlet/utils/Argon.java
+++ src/main/java/com/example/schoolservlet/utils/Argon.java
@@ -2,13 +2,20 @@ package com.example.schoolservlet.utils;
 
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
+import io.github.cdimascio.dotenv.Dotenv;
 
 
 public class Argon {
 
     private static final Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+    private static final Dotenv dotenv = Dotenv.configure()
+                                            .ignoreIfMissing()
+                                            .load();
+    private static final String secret = dotenv.get("SECRET") == null?"":dotenv.get("SECRET");
 
     public static String hash(String password){
+        password = password.concat(secret);
+
         char[] pwd = password.toCharArray();
         try {
             return argon2.hash(3, 65536, 1, pwd);
@@ -18,6 +25,8 @@ public class Argon {
     }
 
     public static boolean verify(String hash, String password){
+        password = password.concat(secret);
+
         char[] pwd = password.toCharArray();
         try {
             return argon2.verify(hash, pwd);
