2be6dca7bf3bffffe5c372c31803da760672c075
eduardocostaam <eduardocosta942z@gmail.com>
2026-03-07 00:06:23 -0300

    fix: build Argon2id instead of Argon2i

--- src/main/java/com/example/schoolservlet/utils/Argon.java
+++ src/main/java/com/example/schoolservlet/utils/Argon.java
@@ -6,7 +6,7 @@ import de.mkammerer.argon2.Argon2Factory;
 
 public class Argon {
 
-    private static final Argon2 argon2 = Argon2Factory.create();
+    private static final Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
 
     public static String hash(String password){
         char[] pwd = password.toCharArray();
