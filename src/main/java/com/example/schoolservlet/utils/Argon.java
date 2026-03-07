83bfcbaa792a235e48a2a1537b7f2c592bb9b3ab
eduardocostaam <eduardocosta942z@gmail.com>
2026-03-06 23:58:48 -0300

    refactor: implement Argon2 instead of Bcrypt

---
+++ src/main/java/com/example/schoolservlet/utils/Argon.java
@@ -0,0 +1,28 @@
+package com.example.schoolservlet.utils;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+
+
+public class Argon {
+
+    private static final Argon2 argon2 = Argon2Factory.create();
+
+    public static String hash(String password){
+        char[] pwd = password.toCharArray();
+        try {
+            return argon2.hash(3, 65536, 1, pwd);
+        } finally {
+            argon2.wipeArray(pwd);
+        }
+    }
+
+    public static boolean verify(String hash, String password){
+        char[] pwd = password.toCharArray();
+        try {
+            return argon2.verify(hash, pwd);
+        } finally {
+            argon2.wipeArray(pwd);
+        }
+    }
+}
