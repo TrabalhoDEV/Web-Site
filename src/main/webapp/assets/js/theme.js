b7631cc5b639f6a757f2ad192d69d7f3943228df
Eduardo Costa <112218438+EduardoCosta942@users.noreply.github.com>
2026-03-15 13:01:50 -0300

    Revert "fix: solving a problem with site's theme (#183)"
    This reverts commit 459bb5977135257f73277701de07c30788eb2494.

--- src/main/webapp/assets/js/theme.js
+++ src/main/webapp/assets/js/theme.js
@@ -1,22 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const themeBtn = document.getElementById('theme-button');
-    const body = document.body;
+        const themeBtn = document.getElementById('theme-button');
+        const body = document.body;
+        if (localStorage.getItem('system-theme') === 'dark') {
+            body.classList.add('dark-mode');
+        }
+        if (themeBtn) {
+            themeBtn.addEventListener('click', () => {
+                body.classList.toggle('dark-mode');
 
-    if (localStorage.getItem('system-theme') === 'dark') {
-        body.classList.add('dark-mode');
+                if (body.classList.contains('dark-mode')) {
+                    localStorage.setItem('system-theme', 'dark');
+                }
+                else {
+                    localStorage.setItem('system-theme', 'light');
+                }
+            });
+        }
+        else {
+            console.error("The 'theme-button' was not found in HTML");
+        }
     }
-
-    if (themeBtn) {
-        themeBtn.addEventListener('click', () => {
-
-            body.classList.toggle('dark-mode');
-            if (body.classList.contains('dark-mode')) {
-                localStorage.setItem('system-theme', 'dark');
-            } else {
-                localStorage.setItem('system-theme', 'light');
-            }
-        });
-    } else {
-        console.error("The 'theme-button' was not found in HTML");
-    }
-});
+)
